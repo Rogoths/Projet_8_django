2d933eaa26f6603e116045875ca1071909275b97
Rogoths <r.muny@hotmail.fr>
2019-01-14 23:36:34 +0100

    add method to request products

--- pur_beurre/management/commands/openfoodfacts.py
+++ pur_beurre/management/commands/openfoodfacts.py
@@ -19,3 +19,42 @@ class Command(BaseCommand):
         for categories in data_categories[0:NB_CAT_REQUEST]:
             cat_prod = Categories.objects.create(name=categories["name"])
             print(cat_prod)
+
+
+    def openfoodfacts_produits(cat_id):
+        """read open food facts api and select id name brand grade detail
+        from a selected categorie"""
+        nb_page = 1
+        while nb_page <= NB_PROD_REQUEST/NB_PROD_PAGE:
+            url = requests.get("https://fr.openfoodfacts.org/langue/francais/categorie/"+str(cat_id)+"/"+str(nb_page)+".json")
+
+            data_raw = url.json()
+            data_produits = data_raw["products"]
+            prod_dict = {}
+            nb_page = nb_page +1
+            for produits in data_produits:
+
+                try:
+                    name_prod = (produits["product_name"])
+                    id_prod = (produits["_id"])
+                    brand_prod = (produits["brands"])
+                    grade_prod = (produits["nutrition_grades"])
+                    detail_prod = (produits["generic_name_fr"])
+                    stores_prod = (produits["stores"])
+                    url_prod = "https://fr.openfoodfacts.org/produit/"+str(produits["_id"])
+                    cat_prod = cat_id[0][0]
+
+                    prod_dict[id_prod] = name_prod, brand_prod, grade_prod, detail_prod, stores_prod, url_prod, cat_prod
+
+                except Exception as e:
+                    pass
+
+            for prod_id, prod in prod_dict.items():
+                name, brand, grade, detail, stores, url, cat = prod
+                try:
+                    insert_products_db(prod_id, name, brand, grade, detail, stores, url, cat)
+                except Exception as e:
+                    print(e)
+
+    if __name__ == '__main__':
+        openfoodfacts_produits("barres")
